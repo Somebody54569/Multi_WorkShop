f579f4d3273932234441b345a5b6140efba0b67e
Sorawis Leepiyasakulchai <sorawis.leep@bumail.net>
2024-03-28 22:53:47 +0700

    Restoring Health

--- Assets/Scripts/Core/Combat/HealingZone.cs
+++ Assets/Scripts/Core/Combat/HealingZone.cs
@@ -17,7 +17,34 @@ public class HealingZone : NetworkBehaviour
     [SerializeField] private int coinsPerTick = 10;
     [SerializeField] private int healthPerTick = 10;
 
+    private float remainingCooldown;
+    private float tickTimer;
+
     private List<TankPlayer> playersInZone = new List<TankPlayer>();
+
+    private NetworkVariable<int> HealPower = new NetworkVariable<int>();
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsClient)
+        {
+            HealPower.OnValueChanged += HandleHealPowerChanged;
+        }
+
+        if (IsServer)
+        {
+            HealPower.Value = maxHealPower;
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsClient)
+        {
+            HealPower.OnValueChanged -= HandleHealPowerChanged;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (!IsServer) { return; }
@@ -34,5 +61,50 @@ public class HealingZone : NetworkBehaviour
         
         playersInZone.Remove(player);
         Debug.Log($"Left: {player.PlayerName.Value}");
+    }
+
+    private void Update()
+    {
+        if (!IsServer) { return; }
+        
+        if (remainingCooldown > 0f)
+        {
+            remainingCooldown -= Time.deltaTime;
+
+            if (remainingCooldown <= 0f)
+            {
+                HealPower.Value = maxHealPower;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        tickTimer += Time.deltaTime;
+        if (tickTimer >= 1/healTickRate)
+        {
+            foreach (TankPlayer player in playersInZone)
+            {
+                if (HealPower.Value == 0 ) { break; }
+                if (player.Health.CurrentHealth.Value == player.Health.MaxHealth) { continue; }
+                if (player.Wallet.TotalCoins.Value < coinsPerTick) { continue; }
+                player.Wallet.SpendCoins(coinsPerTick);
+                player.Health.RestoreHealth(healthPerTick);
+
+                HealPower.Value -= 1;
+                if (HealPower.Value == 0)
+                {
+                    remainingCooldown = healCooldown;
+                }
+            }
+
+            tickTimer = tickTimer % (1 / healTickRate);
+        }
+    }
+
+    private void HandleHealPowerChanged(int oldHealPower, int newHealPower)
+    {
+        healPowerBar.fillAmount = (float)newHealPower / maxHealPower;
     }
 }
